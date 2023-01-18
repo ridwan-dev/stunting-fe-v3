5577f178b703609e83573ed4208d2e1cd1ced004
ridwan.develop <ridwan.develop@gmail.com>
2023-01-19 01:04:27 +0700

    update

--- js/utility/api.js
+++ js/utility/api.js
@@ -1,8 +1,8 @@
 export const api_url = () => {
-  return 'https://monev-stunting.bappenas.go.id/api-testing-v3/api/v1';
+  return 'https://monev-stunting.bappenas.go.id/api-dev-v3/api/v1';
 }
 export const api_url_v3 = () => {
-  return 'https://monev-stunting.bappenas.go.id/api-testing-v3/api/v3';
+  return 'https://monev-stunting.bappenas.go.id/api-dev-v3/api/v3';
 }
 export const base_url = () => {
   return '';
